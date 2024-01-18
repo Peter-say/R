0111be5aa41257b3887072aa2a2a70d05e337f4f
Iriogbe Peter <iriogbepeter22@gmail.com>
2024-01-18 09:06:50 +0100

    spatie permission package installed

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Web\IndexController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [IndexController::class, 'index']);
+Route::prefix('web')->as('web.')->group(function () {
 });
