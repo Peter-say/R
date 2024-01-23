464b029c93bf5710837fbe0039e07630c27b94e1
Iriogbe Peter <iriogbepeter22@gmail.com>
2024-01-23 11:56:24 +0100

    dashboard implemented

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\Dashboard\HomeController;
+use App\Http\Controllers\Dashboard\User\ProfileController;
 use App\Http\Controllers\Web\IndexController;
 use App\Http\Controllers\Web\PropertyController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,4 +22,13 @@ Route::get('/', [IndexController::class, 'index']);
 Route::prefix('property')->as('property.')->group(function () {
     Route::get('listing', [PropertyController::class, 'listing'])->name('listing');
     Route::get('{id}/details', [PropertyController::class, 'details'])->name('details');
+});
+
+Auth::routes();
+Route::prefix('dashboard')->as('dashboard.')->group(function () {
+    Route::get('home', [HomeController::class, 'index'])->name('home');
+
+    Route::prefix('profile')->as('profile.')->group(function () {
+        Route::get('/', [ProfileController::class, 'index']);
+    });
 });
