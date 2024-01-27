8361429b51ed37638d2f65c5ea5c6b4fc41872eb
Iriogbe Peter <iriogbepeter22@gmail.com>
2024-01-27 22:57:16 +0100

    sving state

--- app/Http/Controllers/Dashboard/HomeController.php
+++ app/Http/Controllers/Dashboard/HomeController.php
@@ -4,11 +4,13 @@ namespace App\Http\Controllers\Dashboard;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
    public function index()
    {
-    return view('dashboard.home');
+      $user = Auth::user();
+      return view('dashboard.home');
    }
 }
