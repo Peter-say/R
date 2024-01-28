b86380e52bb64ab4391ac2b23379365629d3215f
Hodley Morrison <eazymorris24@gmail.com>
2024-01-28 01:10:07 +0100

    Default string fixes for mysql conn.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +24,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Schema::defaultStringLength(191);
         view()->composer("*", function ($view) {
             $view->with([
                 'web_assets' => url('/') . env('RESOURCE_URL') . '/web',
